b8f1814d7c5c3a2a8735d2102a2983c7280a4ded
voinuecaterina´@gmail.com <voinuecaterina@gmail.com>
2021-09-10 14:34:27 +0200

    Add info personal

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -15,24 +15,33 @@ function Portfolio() {
         <div className="container">
           <div className="text-wrapper w-full">
             <h1 className="title">Portfolio</h1>
-            <p className="description">
-              Incididunt in cillum magna occaecat nisi qui in.
-            </p>
+            <p className="description">These are some of my projects</p>
 
             <div className="portfolio-wrapper">
+              {/* portfolio items */}
               <div className="portfolio-item">
                 <img src="/chitchat.png" className="portfolio-image" />
 
                 <h4 className="portfolio-name">Chat App Exploration</h4>
                 <div className="portfolio-category">Mobile Dev</div>
               </div>
-              <div className="portfolio-item">
-                <img
-                  src="/kajian.jpguseEffect(() => {useEffect(() => {
-  () => {};
 
-                  className="portfolio-image"
-                />
+              <div className="portfolio-item">
+                <img src="/chitchat.png" className="portfolio-image" />
+
+                <h4 className="portfolio-name">Chat App Exploration</h4>
+                <div className="portfolio-category">Mobile Dev</div>
+              </div>
+
+              <div className="portfolio-item">
+                <img src="/chitchat.png" className="portfolio-image" />
+
+                <h4 className="portfolio-name">Weather app Exploration</h4>
+                <div className="portfolio-category">Web Dev</div>
+              </div>
+
+              <div className="portfolio-item">
+                <img src="/kajian.jpg" className="portfolio-image" />
 
                 <h4 className="portfolio-name">kajian App</h4>
                 <div className="portfolio-category">Mobile Dev</div>
